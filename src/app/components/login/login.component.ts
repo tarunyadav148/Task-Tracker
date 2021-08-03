7992a0c24d0de42bc75f80b62fd1fd7bd6260698
Tarun Singh Yadav <tarunyadav148@gmail.com>
2021-08-03 19:36:19 +0530

    new login page with google,fb,twitter

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -10,7 +10,11 @@ import { DatabaseServiceService } from 'src/app/services/database-service.servic
 })
 export class LoginComponent implements OnInit {
 
-  loginimgsrc: string = 'https://firebasestorage.googleapis.com/v0/b/task-traker-4f454.appspot.com/o/loginwithgoogle1.png?alt=media&token=c54c3f66-e902-4a41-b02f-aec3ffe452bf';
+  loginwithgoogle: string = 'https://firebasestorage.googleapis.com/v0/b/task-traker-4f454.appspot.com/o/loginwithgoogle1.png?alt=media&token=c54c3f66-e902-4a41-b02f-aec3ffe452bf';
+
+  loginwithfb: string = 'https://firebasestorage.googleapis.com/v0/b/task-traker-4f454.appspot.com/o/loginwithfb.png?alt=media&token=09672335-238a-40da-8f80-416aec7ab6ad';
+
+  loginwithtwitter: string = 'https://firebasestorage.googleapis.com/v0/b/task-traker-4f454.appspot.com/o/loginwithtwitter.png?alt=media&token=d194a63e-4ee1-47b2-b5fe-17cef435f432';
 
   constructor(public auth: AngularFireAuth,private DatabaseService:DatabaseServiceService) {}
 
@@ -20,11 +24,24 @@ export class LoginComponent implements OnInit {
     })
   }
 
-  login() {
+  loginWithGoogle() {
     this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(res =>{
       this.DatabaseService.setUserUid(res.user?.uid);
     });
   }
+
+  loginWithFB(){
+    this.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider()).then(res=>{
+      this.DatabaseService.setUserUid(res.user?.uid);
+    });
+  }
+
+  loginWithTwitter() {
+    this.auth.signInWithPopup(new firebase.auth.TwitterAuthProvider()).then(res =>{
+      this.DatabaseService.setUserUid(res.user?.uid);
+    });
+  }
+
   logout() {
      this.auth.signOut();
   }
